748494db269b3aa62414fd037e0d2b63563a4f8e
xiaofu98 <728795603@qq.com>
2020-10-11 09:24:54 +0800

    model added

--- mnist/train.py
+++ mnist/train.py
@@ -8,17 +8,18 @@ import torch
 import torchvision
 from torchvision import datasets, transforms
 from torch.autograd import Variable
-
+import model
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.5], std=[0.5])])
-
 data_train = datasets.MNIST(root="./data/",
                             transform=transform,
                             train=True,
                             download=True)
-
 data_test = datasets.MNIST(root="./data/",
                            transform=transform,
                            train=False)
 data_loader_train = torch.utils.data.DataLoader(dataset=data_train, batch_size=64, shuffle=True)
-
 data_loader_test = torch.utils.data.DataLoader(dataset=data_test, batch_size=64, shuffle=True)
+
+model = model()
+cost = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
